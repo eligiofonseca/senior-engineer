72cddc4d075172923b8d95297d03675c29a7e0ae
eligiofonseca <eligio.fonseca@gmail.com>
2023-12-12 20:06:08 -0800

    adding header and footer to main js instead

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
+class MyHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="./index.html" target='_self'>Home</a></li>
+                    <li><a href="./about.html" target='_self'>About</a></li>
+                    <li><a href="./media.html" target='_self'>Media</a></li>
+                    <li><a href="./contactme.html" target='_self'>Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+        `
+    }
+}
+customElements.define('my-header', MyHeader)
+
+class MyFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <footer>
+            <p>&copy; 2023 My Website. All rights reserved.</p>
+        </footer>
+        `
+    }
+}
+customElements.define('my-footer', MyFooter)
+
 //Option 1 - Contact Form Submission Success Event Listener
 window.addEventListener("load", function () {
     const form = document.getElementById('my-form');
