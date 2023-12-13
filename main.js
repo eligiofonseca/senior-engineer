5e92b516b748d1170fcd6c19b5350c78837a61b5
eligiofonseca <eligio.fonseca@gmail.com>
2023-12-12 18:43:59 -0800

    form js changes

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 //document.getElementsByTagName("h2")[0].style.fontSize = "80%";
+
+//Contact Form Submission Success Event Listener
+window.addEventListener("load", function () {
+    const form = document.getElementById('my-form');
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const data = new FormData(form);
+        const action = e.target.action;
+        fetch(action, {
+            method: 'POST',
+            body: data,
+        })
+            .then(() => {
+                alert("Success!");
+            })
+    });
+});
