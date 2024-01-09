a87b8a418d350ccc2b2b8b73da00bfbeaf086184
eligiofonseca <eligio.fonseca@gmail.com>
2024-01-09 15:48:29 -0800

    added Portfolio page and cleaned up some html/css

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 class MyHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
+        <header>
             <nav>
                 <ul>
                     <li><a href="./index.html" target='_self'>Home</a></li>
                     <li><a href="./about.html" target='_self'>About</a></li>
                     <li><a href="./media.html" target='_self'>Media</a></li>
+                    <li><a href="./portfolio.html" target='_self'>Portfolio</a></li>
                     <li style="float:right"><a href="./contactme.html" target='_self'>Contact Me</a></li>
                 </ul>
             </nav>
+        </header>
         `
     }
 }
@@ -17,7 +20,6 @@ customElements.define('my-header', MyHeader)
 class MyFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-
         <footer>
         <figure class='footerIcons'>
           <a href='./index.html' target='_self'>
