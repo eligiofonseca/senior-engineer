d730235c5943dddb0df72a00489c95a024f72b9b
eligiofonseca <eligio.fonseca@gmail.com>
2024-01-09 12:06:55 -0800

    mini revamp of entire website

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 class MyHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <header>
             <nav>
                 <ul>
                     <li><a href="./index.html" target='_self'>Home</a></li>
                     <li><a href="./about.html" target='_self'>About</a></li>
                     <li><a href="./media.html" target='_self'>Media</a></li>
-                    <li><a href="./contactme.html" target='_self'>Contact Me</a></li>
+                    <li style="float:right"><a href="./contactme.html" target='_self'>Contact Me</a></li>
                 </ul>
             </nav>
-        </header>
         `
     }
 }
@@ -19,8 +17,26 @@ customElements.define('my-header', MyHeader)
 class MyFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
+
         <footer>
-            <p>&copy; 2023 My Website. All rights reserved.</p>
+        <figure class='footerIcons'>
+          <a href='./index.html' target='_self'>
+          <img src='./icons/footer/iconmonstr-home-6.svg'>
+          </a>
+
+          <a href='https://www.linkedin.com/in/eligio-fonseca/' target='_blank'>
+          <img src='./icons/footer/iconmonstr-linkedin-3.svg'>
+          </a>
+
+          <a href='https://github.com/eligiofonseca' target='_blank'>
+          <img src='./icons/footer/iconmonstr-github-1.svg'>
+          </a>
+
+          <a href='https://www.codecademy.com/profiles/eligiofonseca' target='_blank'>
+          <img src='../icons/footer/iconmonstr-learning-3.svg'>
+          </a>
+        </figure>
+            &copy; 2023 All Rights Reserved.
         </footer>
         `
     }
