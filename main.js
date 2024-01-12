ac107737100c5cb1c9938eae233440668cd18072
eligiofonseca <eligio.fonseca@gmail.com>
2024-01-12 14:56:42 -0800

    nav menu cleanup and fixes for smaller devices

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ class MyHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <header>
-            <nav>
+            <nav id='menu'>
                 <ul>
                     <li><a href="./index.html" target='_self'>Home</a></li>
                     <li><a href="./about.html" target='_self'>About</a></li>
                     <li><a href="./portfolio.html" target='_self'>Portfolio</a></li>
-                    <!-- <li><a href="./media.html" target='_self'>Media</a></li> -->
+                    <li><a href="./media.html" target='_self'>Media</a></li>
                     <li style="float:right"><a href="./contactme.html" target='_self'>Contact Me</a></li>
                 </ul>
             </nav>
