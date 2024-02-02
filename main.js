01e220b33ed08194ac6fe3703e81c71329e47c56
eligiofonseca <eligio.fonseca@gmail.com>
2024-02-02 14:07:00 -0800

    media page changes and making it public.

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ class MyHeader extends HTMLElement {
                     <!-- <li><a href="./index.html" target='_self'>Home</a></li> -->
                     <li><a href="./about.html" target='_self'>About</a></li>
                     <!-- <li><a href="./portfolio.html" target='_self'>Portfolio</a></li> -->
-                    <!-- <li><a href="./media.html" target='_self'>Media</a></li> -->
+                    <li><a href="./media.html" target='_self'>Media</a></li>
                     <li><a href="./contactme.html" target='_self'>Contact Me</a></li>
                 </ul>
             </nav>
@@ -20,7 +20,7 @@ customElements.define('my-header', MyHeader)
 class MyFooter extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-    <button onclick="topFunction()" id="bttb" title="Back to the Top">Top
+    <button onclick="topFunction()" id="bttb" title="Back to the Top">Top &uarr;   
     </button>
         <footer>
         <figure class='footerIcons'>
