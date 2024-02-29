f397d3bd20f59b0b566855af8acf6e676ae5fa21
eligiofonseca <eligio.fonseca@gmail.com>
2024-02-29 15:05:38 -0800

    hid media portion of my professional portfolio website.

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ class MyHeader extends HTMLElement {
                     <!-- <li><a href="./index.html" target='_self'>Home</a></li> -->
                     <li><a href="./about.html" target='_self'>About</a></li>
                     <!-- <li><a href="./portfolio.html" target='_self'>Portfolio</a></li> -->
-                    <li><a href="./media.html" target='_self'>Media</a></li>
+                    <!-- <li><a href="./media.html" target='_self'>Media</a></li> -->
                     <li><a href="./contactme.html" target='_self'>Contact Me</a></li>
                 </ul>
             </nav>
